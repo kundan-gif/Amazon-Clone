43740e4a9b368d7376201ef1ab2d54332d478096
pravin5551 <pravinvpawar5551@gmail.com>
2021-02-02 20:44:54 +0530

    Login page and SignUp page

--- app/src/main/java/com/example/amazonclone/Activities/SignInActivity.java
+++ app/src/main/java/com/example/amazonclone/Activities/SignInActivity.java
@@ -2,15 +2,33 @@ package com.example.amazonclone.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.amazonclone.R;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity{
+    private Button mBtnAlreadySignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        initVie();
     }
+
+    private void initVie() {
+        mBtnAlreadySignin = findViewById(R.id.btnAlreadyCustomer);
+        mBtnAlreadySignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, LoginActivityP.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
