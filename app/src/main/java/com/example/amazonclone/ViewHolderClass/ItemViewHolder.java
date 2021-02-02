3c6d7905711749c65e84ee7a0f9a80cff9bb3239
Kundan Kumar <kundan211097@gmail.com>
2021-02-02 15:35:39 +0530

    Landing page-Language Activity

---
+++ app/src/main/java/com/example/amazonclone/ViewHolderClass/ItemViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.amazonclone.ViewHolderClass;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
