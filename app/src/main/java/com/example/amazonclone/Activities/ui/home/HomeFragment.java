c69fa6875df60a6f62ea308fa3f0ccb9d77d1988
pravin5551 <pravinvpawar5551@gmail.com>
2021-02-04 13:48:59 +0530

    Home page layout with ImageSlider

--- app/src/main/java/com/example/amazonclone/Activities/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/amazonclone/Activities/ui/home/HomeFragment.java
@@ -4,21 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.amazonclone.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private Toolbar toolbar;
+
+//    int[] images = {R.drawable.imageone, R.drawable.imagetwo, R.drawable.imagethree};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +36,38 @@ public class HomeFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+
         return root;
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageSlider imageSlider = view.findViewById(R.id.slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.imagesoffer,""));
+        slideModels.add(new SlideModel(R.drawable.imagesix,""));
+        slideModels.add(new SlideModel(R.drawable.imagesfivee,""));
+        slideModels.add(new SlideModel(R.drawable.lol,""));
+        slideModels.add(new SlideModel(R.drawable.imagesfivee,""));
+        slideModels.add(new SlideModel(R.drawable.lolt,""));
+        slideModels.add(new SlideModel(R.drawable.lolthree,""));
+        slideModels.add(new SlideModel(R.drawable.lolfour,""));
+        slideModels.add(new SlideModel(R.drawable.imagefour,""));
+        slideModels.add(new SlideModel(R.drawable.imagesix,""));
+        imageSlider.setImageList(slideModels,true);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
